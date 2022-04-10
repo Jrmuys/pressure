2b970e330a00bcc7b71781111d04c88d38304516
Joel Muyskens <jrmuyskens@gmail.com>
2022-04-10 19:12:27 -0400

    added .env

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,12 +9,14 @@ import axios from 'axios';
 //   response.send("Hello from Firebase!");
 // });
 
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
 
 const lat = 41.654282;
 const lon = -91.568222;
 
-export const scheduledWeatherData = functions.pubsub
+export const scheduledWeatherDataGR = functions.pubsub
   .schedule('*/5 * * * *')
   .onRun(async (context) => {
     const db = admin.firestore();
@@ -32,6 +34,36 @@ export const scheduledWeatherData = functions.pubsub
       let data = res.data;
       functions.logger.log('Data:', data);
       await db.collection('weather').add(data);
+
+      functions.logger.log(`Logged data correctly on ${new Date()}`);
+      return null;
+    } catch (error) {
+      functions.logger.log(`Error logging data: ${error}`);
+      return null;
+    }
+  });
+
+const pittLat = 40.4443533;
+const pittLong = -79.9630237;
+
+export const scheduledWeatherDataPitt = functions.pubsub
+  .schedule('*/5 * * * *')
+  .onRun(async (context) => {
+    const db = admin.firestore();
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${pittLat}&lon=${pittLong}&appid=${process.env.OPEN_WEATHER_API_KEY}`;
+    // const options = {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    // };
+
+    try {
+      let res = await axios.get(url);
+      functions.logger.info(res);
+      let data = res.data;
+      functions.logger.log('Data:', data);
+      await db.collection('weatherPitt').add(data);
 
       functions.logger.log(`Logged data correctly on ${new Date()}`);
       return null;
